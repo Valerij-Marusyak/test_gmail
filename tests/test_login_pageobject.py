491fb684e214c72853eb3225595c78a0dc7a89bd
Valerij-Marusyak <116340537+Valerij-Marusyak@users.noreply.github.com>
2022-11-18 21:22:58 +0200

    Update test_login_pageobject.py

--- tests/test_login_pageobject.py
+++ tests/test_login_pageobject.py
@@ -17,11 +17,10 @@ class LoginTests(unittest.TestCase):
 
     def setUp(self) -> None:
         self.driver = WebDriverFactory.get_driver()
-        # self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.get('http://mail.google.com')
         self.login_page = LoginPage(self.driver)
         self.login_page.wait_for_page_is_loaded()
-        self.login_page.set_english_local()
+        #self.login_page.set_english_local()
 
     def tearDown(self) -> None:
         self.driver.save_screenshot('test-reports/' + self.id() + '.png')
@@ -29,46 +28,41 @@ class LoginTests(unittest.TestCase):
 
     def test_valid_values(self):
         """ valid email and valid password """
-        """
         self.login_page.enter_email(self.email)
         self.login_page.next_button_click()
         self.login_page.enter_password(self.password)
         self.login_page.password_next_button_click()
-        self.assertTrue(self.login_page.wait_for_account_is_present())"""
+        self.assertTrue(self.login_page.wait_for_account_is_present())
 
     def test_valid_email_and_incorrect_password(self):
         """ valid email and incorrect password """
-        """
         self.login_page.enter_email(self.email)
         self.login_page.next_button_click()
         self.login_page.enter_password(self.password_incorrect)
         self.login_page.password_next_button_click()
         alert = self.login_page.alert_password_incorrect()
-        self.assertIn('Wrong password.', alert)"""
+        self.assertIn('Wrong password.', alert)
 
     def test_incorrect_email(self):
         """ incorrect email """
-        """
         self.login_page.enter_email(self.email_incorrect)
         self.login_page.next_button_click()
         alert = self.login_page.alert_email_incorrect()
-        self.assertEqual('Try again', alert)"""
+        self.assertEqual('Try again', alert)
 
     def test_forgot_password(self):
         """ Verify the ‘Forgot Password’ functionality """
-        """
         self.login_page.enter_email(self.email)
         self.login_page.next_button_click()
         self.login_page.link_forgot_password_click()
         alert = self.login_page.get_alert_sms()
-        self.assertEqual('SMS', alert)"""
+        self.assertEqual('SMS', alert)
 
     def test_forgot_email(self):
         """ Verify the ‘Forgot Email’ functionality """
-        """
         self.login_page.link_forgot_email_click()
         alert = self.login_page.get_alert_number_of_phone()
-        self.assertEqual('Enter your phone number or recovery email', alert)"""
+        self.assertEqual('Enter your phone number or recovery email', alert)
 
     ''' 
     I have implemented the basic test cases, imho :). 
